4b502b007e08b60e6cda43be3d72fb1994325c12
Justin Toffel <justin@biftin.moe>
2023-03-06 23:16:00 +0100

    implement getting entity states

--- pkg/hass-client/hass-client.go
+++ pkg/hass-client/hass-client.go
@@ -4,10 +4,25 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
-	"fmt"
 
 	"nhooyr.io/websocket"
 )
+
+var InvalidResponseError = errors.New("invalid response")
+
+type EntityState struct {
+	id         string
+	state      string
+	attributes map[string]interface{}
+}
+
+func (entity *EntityState) ID() string {
+    return entity.id
+}
+
+func (entity *EntityState) State() string {
+    return entity.state
+}
 
 type HassClient struct {
 	ctx  context.Context
@@ -38,28 +53,64 @@ func Connect(ctx context.Context, server, token string) (*HassClient, error) {
 	return &client, nil
 }
 
-func (client *HassClient) GetStates() {
+func (client *HassClient) GetStates() ([]EntityState, error) {
 	cmdId := client.cmdId
 	client.cmdId++
 
-	msgData := make(map[string]interface{})
-	msgData["id"] = cmdId
-	msgData["type"] = "get_states"
+	msgData := map[string]interface{}{
+		"id":   cmdId,
+		"type": "get_states",
+	}
 
 	err := client.sendJson(&msgData)
 	if err != nil {
-		fmt.Println(err)
-		return
+		return nil, err
 	}
 
-	msgData = make(map[string]interface{})
+	msgData = map[string]interface{}{}
 	err = client.readJson(&msgData)
 	if err != nil {
-		fmt.Println(err)
-		return
+		return nil, err
 	}
 
-	fmt.Println(msgData)
+	success, ok := readBool(msgData, "success")
+	if !ok {
+		return nil, InvalidResponseError
+	}
+
+	if !success {
+		return nil, readError(msgData)
+	}
+
+	result, ok := readSlice(msgData, "result")
+	if !ok {
+		return nil, InvalidResponseError
+	}
+
+	entities := []EntityState{}
+
+	for _, resultItem := range result {
+		if resultItem, ok := resultItem.(map[string]interface{}); ok {
+			id, ok := readString(resultItem, "entity_id")
+			if !ok {
+				return nil, InvalidResponseError
+			}
+
+			state, ok := readString(resultItem, "state")
+			if !ok {
+				return nil, InvalidResponseError
+			}
+
+			attributes, ok := readMap(resultItem, "attributes")
+			if !ok {
+				return nil, InvalidResponseError
+			}
+
+			entities = append(entities, EntityState{id, state, attributes})
+		}
+	}
+
+	return entities, nil
 }
 
 func (client *HassClient) Close() {
@@ -108,9 +159,11 @@ func (client *HassClient) authenticate(token string) error {
 		return errors.New("received unexpected message type: " + msgData["type"])
 	}
 
-	msgData = make(map[string]string)
-	msgData["type"] = "auth"
-	msgData["access_token"] = token
+	msgData = map[string]string{
+		"type":         "auth",
+		"access_token": token,
+	}
+
 	err = client.sendJson(&msgData)
 	if err != nil {
 		return err
@@ -130,4 +183,63 @@ func (client *HassClient) authenticate(token string) error {
 	}
 
 	return nil
+}
+
+func readBool(data map[string]interface{}, key string) (bool, bool) {
+	raw, ok := data[key]
+	if !ok {
+		return false, false
+	}
+
+	output, ok := raw.(bool)
+	return output, ok
+}
+
+func readString(data map[string]interface{}, key string) (string, bool) {
+	raw, ok := data[key]
+	if !ok {
+		return "", false
+	}
+
+	output, ok := raw.(string)
+	return output, ok
+}
+
+func readSlice(data map[string]interface{}, key string) ([]interface{}, bool) {
+	raw, ok := data[key]
+	if !ok {
+		return nil, false
+	}
+
+	output, ok := raw.([]interface{})
+	return output, ok
+}
+
+func readMap(data map[string]interface{}, key string) (map[string]interface{}, bool) {
+	raw, ok := data[key]
+	if !ok {
+		return nil, false
+	}
+
+	output, ok := raw.(map[string]interface{})
+	return output, ok
+}
+
+func readErrorMessage(data map[string]interface{}) (string, bool) {
+	errorMap, ok := readMap(data, "error")
+	if !ok {
+		return "", false
+	}
+
+	errorString, ok := readString(errorMap, "message")
+	return errorString, ok
+}
+
+func readError(data map[string]interface{}) error {
+	errorMessage, ok := readErrorMessage(data)
+	if !ok {
+		return InvalidResponseError
+	}
+
+	return errors.New(errorMessage)
 }
